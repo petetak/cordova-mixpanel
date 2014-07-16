59e49e81c94d8e1e476715ea9bcd4d450a7590b2
Pete Nelson <pete.nelson@mac.com>
2014-07-16 14:56:10 +0100

    change plugin.xml

--- src/android/com/mixpanel/android/CDVMixpanel.java
+++ src/android/com/mixpanel/android/CDVMixpanel.java
@@ -1,5 +1,5 @@
 package com.mixpanel.android;
-
+import com.mixpanel.android.mpmetrics;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
@@ -20,9 +20,19 @@ public class CDVMixpanel extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if ("init".equals(action)) {
-            System.out.println("hello world "+action);
-            callbackContext.success();
-            return true;
+                    try{
+                        MixpanelAPI mixpanel = MixpanelAPI.getInstance(this.cordova.getActivity().getApplicationContext(), data.getString(0));
+                        JSONObject props = new JSONObject();    
+                        props.put("Test", "True");
+                        mixpanel.track("Open_App", props);
+                        System.out.println("hello world "+mixpanel+" - "+data.getString(0));
+                        callbackContext.success();
+                    }
+                    catch(Exception e){
+                        callbackContext.error("Error is Mixpanel "+e);
+                        
+                    }
+          return true;
         }
         return false;  // Returning false results in a "MethodNotFound" error.
     }
